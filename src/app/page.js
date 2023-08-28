54605d6fe8e4946e76c560ef5b014542ee512988
tensept <vankaew58@gmail.com>
2023-08-28 16:17:03 +0700

    lab14

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,37 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Button, Container, Divider, Group, Pagination, Rating, Text, Textarea, Title } from "@mantine/core";
+import { useState } from "react";
 
 export default function FoodReviewPage() {
+  //const [value, setValue] = useState(0);
+
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
-
+      <Title order={4} mt="sm">Your rating</Title>
+      <Rating size="lg" />
+      <Textarea 
+        label = "Your review"
+        placeholder="Do you enjoy eating?"
+        mt="xs"
+        minRows={3}
+      />
+      <Button mt="xs" color="orange">Submit Review</Button>
+      <Divider my="sm" />
+      <Group position="center">
+      <Title order={4}>Elon Musk</Title>
+      <Rating size="sm" value={5} readOnly/>
+      </Group> 
+      <Text ta="center" c="dimmed">Best pizza in this world. I give you X score.</Text>
+      <Divider my="sm" />
+      <Group position="center">
+      <Title order={4}>Mark Zuck</Title>
+      <Rating size="sm" value={4} readOnly/>
+      </Group> 
+      <Text ta="center" c="dimmed">My favourite part is pepperoni</Text>
+      <Pagination total={20} color="orange" position="center" mt="md"/>
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Waenkaew Phimsena 650610809
       </Text>
     </Container>
   );
